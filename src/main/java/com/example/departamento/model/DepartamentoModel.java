f1fb5b524587f03741b87fc92a569ddf13db1a8e
PedroCavalini <pedrogameleira01@gmail.com>
2026-03-24 16:56:57 -0300

    Incluindo DepartamentoModel

---
+++ src/main/java/com/example/departamento/model/DepartamentoModel.java
@@ -0,0 +1,44 @@
+package com.example.departamento.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Departamento")
+@Table(name = "tb_departamento")
+public class DepartamentoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nm_departamento")
+    private String nome;
+
+    @Column(name = "ds_localizacao")
+    private String localizacao;
+
+    public DepartamentoModel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
